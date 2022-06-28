5d6ad6a1454103b5a3729b0b62b5b716ee4cd34b
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-06-28 17:38:12 +0700

    gvbhygvb

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleRandomizer
 {
@@ -7,9 +9,7 @@ namespace ConsoleRandomizer
         public static void Main(string[] args)
         {
             // Console window settings
-            #pragma warning disable CA1416 // Validate platform compatibility
-            Console.WindowHeight = 20;
-            #pragma warning restore CA1416 // Validate platform compatibility
+            Console.Title = "Press any key";
 
             // Program
             Randomize();
@@ -22,13 +22,18 @@ namespace ConsoleRandomizer
             // Arrays
             ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
             string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            string title;
 
             // Main
             while (true)
             {
                 Console.ReadKey();
+
+                // Randomizes text and background colors
                 Console.ForegroundColor = colors[randGen.Next(colors.Length)];
                 Console.BackgroundColor = colors[randGen.Next(colors.Length)];
+
+                // Writes stuff
                 for(int i = 1; i < randGen.Next(1, 101); i++)
                 {              
                     for(i = 0; i < randGen.Next(2000, 6001); i++)
@@ -36,7 +41,11 @@ namespace ConsoleRandomizer
                         Console.Write(chars[randGen.Next(1, 61)]);
                     }
                     Console.WriteLine(chars[randGen.Next(1, 61)]);
-                    Console.Title = (chars[randGen.Next(1, 61)]);
+                }
+                for (int i = 0; i < randGen.Next(1, 101); i++)
+                {
+                    title = chars[randGen.Next(1, 61)];
+                    Console.Title = title + chars[randGen.Next(1, 61)]
                 }
             }
         }
